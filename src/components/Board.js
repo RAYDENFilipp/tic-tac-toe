1155f658a05e23fb433a0a20fd6ff65448729094
Philipp Lypniakov <filipprayden@gmail.com>
2018-09-17 22:00:28 +0300

    Done

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Square from '../components/Square';
 
 export default class Board extends Component {
+    
     renderSquare(i) {
         return (
             <Square
@@ -9,26 +10,28 @@ export default class Board extends Component {
                 onClick={() => this.props.onClick(i)} />
         );
     }
+
+    renderBoard(row, col) {
+        const board = [];
+        let cellCounter = 0;
+
+        for (let i = 0; i < row; i++) {
+            const columns = [];
+            for (let j = 0; j < col; j++) {
+                columns.push(this.renderSquare(cellCounter++));
+            }
+            board.push(
+                <div key={i} className='board-row'>
+                    {columns}
+                </div>
+            );
+        }
+        return board;
+    }
 // call calculateWinner(squares) in the Board’s render function to check if a player has won. If a player has won, we can display text such as “Winner: X” or “Winner: O”.
     render() {
         return (
-            <div>
-                <div className='board-row'>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className='board-row'>
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className='board-row'>
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+            <div>{this.renderBoard(3, 3)}</div>
+        )
     }
 }
